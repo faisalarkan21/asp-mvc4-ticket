4f992fbdb84eb6c3ca1885a8f33ff701b0487387
faisal arkan <faisalarkan21@gmail.com>
2017-08-16 01:09:27 +0700

    send validation ticket

--- tiket_airlines/Controllers/UserController.cs
+++ tiket_airlines/Controllers/UserController.cs
@@ -35,17 +35,11 @@ namespace tiket_airlines.Controllers
         public ActionResult data_pembeli(int id, Gabungan gabungan)
         {
 
-
-
-
             var user = db.pembeli.FirstOrDefault(u => u.id_pembeli == id);
             user.nm_pembeli = gabungan.tblPembeli.nm_pembeli;
             user.email_pembeli = gabungan.tblPembeli.email_pembeli;
             user.hp_pembeli = gabungan.tblPembeli.hp_pembeli;
             db.SaveChanges();
-
-
-
 
             return RedirectToAction("data_pembeli", "User");
         }
@@ -59,7 +53,32 @@ namespace tiket_airlines.Controllers
 
         public ActionResult validasi_tiket()
         {
-            return View();
+
+
+            Gabungan gabungan = new Gabungan();
+            int idUser = (int)Session["id"];
+            //Gabungan. student = db.Students.Find(id);
+            gabungan.tblPembeli = db.pembeli.Find(idUser);
+            gabungan.tblDetailTiket = db.detil_pesan_tiket.Find(idUser);
+            gabungan.tblValidasi = db.pembeli_validasi.Find(idUser);
+
+            return View(gabungan);
+
         }
+
+        [HttpPost]
+        public ActionResult validasi_tiket(int id, Gabungan gabungan)
+        {
+
+
+            var userValidasi = db.pembeli_validasi.FirstOrDefault(u => u.id_pembeli == id);
+            userValidasi.pilihan_bank = gabungan.tblDetailTiket.pilihan_bank;
+            userValidasi.uang_transfer_validasi = gabungan.tblDetailTiket.harga_tiket;
+            db.SaveChanges();
+
+            return RedirectToAction("validasi_tiket", "User");
+
+        }
+
     }
 }
