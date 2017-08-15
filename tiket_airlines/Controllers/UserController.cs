930af810ab77cbdd0696690f8c71e38980f5998d
faisal arkan <faisalarkan21@gmail.com>
2017-08-16 00:07:41 +0700

    update data user and new model entity

--- tiket_airlines/Controllers/UserController.cs
+++ tiket_airlines/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using tiket_airlines.Models;
 using tiket_airlines.Security;
 
 namespace tiket_airlines.Controllers
@@ -10,6 +11,8 @@ namespace tiket_airlines.Controllers
     [AuthorizationFilterUser]
     public class UserController : Controller
     {
+        tiket_airlinesEntitiesMVC db = new tiket_airlinesEntitiesMVC();
+
         // GET: User
         public ActionResult dashboard()
         {
@@ -18,7 +21,33 @@ namespace tiket_airlines.Controllers
 
         public ActionResult data_pembeli()
         {
-            return View();
+
+            Gabungan gabungan = new Gabungan();
+            int idUser = (int)Session["id"];
+            //Gabungan. student = db.Students.Find(id);
+            gabungan.tblPembeli = db.pembeli.Find(idUser);
+            gabungan.tblDetailTiket = db.detil_pesan_tiket.Find(idUser);
+
+            return View(gabungan);
+        }
+
+        [HttpPost]
+        public ActionResult data_pembeli(int id, Gabungan gabungan)
+        {
+
+
+
+
+            var user = db.pembeli.FirstOrDefault(u => u.id_pembeli == id);
+            user.nm_pembeli = gabungan.tblPembeli.nm_pembeli;
+            user.email_pembeli = gabungan.tblPembeli.email_pembeli;
+            user.hp_pembeli = gabungan.tblPembeli.hp_pembeli;
+            db.SaveChanges();
+
+
+
+
+            return RedirectToAction("data_pembeli", "User");
         }
 
 
---
+++ tiket_airlines/Models/Gabungan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tiket_airlines.Models
+{
+    public class Gabungan
+    {
+        public pembeli tblPembeli { get; set; }
+        public pembeli_validasi tblValidasi { get; set; }
+        public detil_pesan_tiket tblDetailTiket { get; set; }
+     
+    }
+}
